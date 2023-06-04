7365ab38493fc88a3b5c0b83da3ba57b622a6ce5
cadbee <aschiam.net@gmail.com>
2023-06-04 13:31:56 +0300

    Import/Export GeoJSON added

--- src/components/CesiumViewer.vue
+++ src/components/CesiumViewer.vue
@@ -2,8 +2,32 @@
     <el-row
             class="el-header centerRow"
             style="background-color: rgba(199,198,198,0.22)"
-            justify="end"
+            justify="space-between"
     >
+        <el-space>
+            <el-button
+                type="primary"
+                size="large"
+                plain
+                @click="saveAsGeoJSON"
+            >
+                Save as GeoJSON
+            </el-button>
+            <el-button
+                type="primary"
+                size="large"
+                plain
+                @click="onGeoJSONUploadButtonClicked"
+            >
+                Import from GeoJSON
+            </el-button>
+            <input
+                style="display: none"
+                ref="uploadGeoJSONInput"
+                type="file"
+                @change="uploadGeoJSON"
+            />
+        </el-space>
         <el-space>
             <p>Draw: </p>
             <el-select
@@ -46,33 +70,21 @@
             </el-button>
         </el-space>
     </el-row>
-    <el-container id="map2d">
-        <el-select
-                class="layer_select"
-                v-model="pickedStyle"
-                placeholder="Select layer"
-                size="large"
-                @change="onLayerChange"
-        >
-            <el-option
-                    v-for="item in layers"
-                    :key="item"
-                    :label="item.imagerySet"
-                    :value="item.imagerySet"
-            />
-        </el-select>
-    </el-container>
+
+    <el-container id="map2d"/>
+
     <el-container id="map3d">
         <el-button
-                class="fs-button"
-                @click="enableCesiumFs"
+            class="cesium-buttons cesium-fs-button"
+            @click="enableCesiumFs"
         >
             Cesium Fs
         </el-button>
         <el-button
-                id="enable"
+            class="cesium-buttons cesium-enable-button"
+            @click="onEnableCesiumButtonClick"
         >
-            {{ isEnabled ? "Disable" : "Enable" }}
+            {{ isCesiumEnabled ? "Disable" : "Enable" }}
         </el-button>
     </el-container>
 </template>
@@ -80,18 +92,19 @@
 <script>
 import "cesium/Build/Cesium/Widgets/widgets.css"
 import * as Cesium from 'cesium'
-
 window.Cesium = Cesium
-
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhN2FjYTI2NC1jOGMwLTQzMTgtYjM1Zi05Y2QxMjBjZWMyZmEiLCJpZCI6MTQxNjc5LCJpYXQiOjE2ODUwOTMxNjl9.b0uZ3zsVeASjaYccL8obTVZFwAmnJm5ZgEE_t_JM-gk'
-
 import OLCesium from 'olcs/OLCesium.js'
+
+import 'ol-ext/dist/ol-ext.css'
+import LayerSwitcherImage from 'ol-ext/control/LayerSwitcherImage'
+
 import Map from 'ol/Map'
 import View from 'ol/View'
 import Draw from 'ol/interaction/Draw'
 import {fromLonLat} from 'ol/proj'
 import TileLayer from "ol/layer/Tile";
-import {Vector as LayerVector} from "ol/layer";
+import {Vector as VectorLayer} from "ol/layer";
 import {BingMaps, Vector as SourceVector} from "ol/source";
 import {Feature} from "ol";
 import {Point} from "ol/geom";
@@ -99,190 +112,250 @@ import {FullScreen, defaults as defaultControls, MousePosition} from 'ol/control
 import {createStringXY} from "ol/coordinate";
 import {Geolocation} from "ol";
 import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style.js';
+import {GeoJSON} from "ol/format";
+
+import {ref, onMounted} from "vue";
+
 export default {
     name: 'CesiumViewer',
-    data() {
-        return {
-            layer: '',
-            isEnabled: false,
-            pickedStyle: 'RoadOnDemand',
-            map: undefined,
-            layers: [
-                {
-                    isVisible: true,
-                    imagerySet: 'RoadOnDemand',
-                },
-                {
-                    isVisible: false,
-                    imagerySet: 'Aerial',
-                },
-                {
-                    isVisible: false,
-                    imagerySet: 'AerialWithLabelsOnDemand',
-                },
-                {
-                    isVisible: false,
-                    imagerySet: 'CanvasDark',
-                },
-                {
-                    isVisible: false,
-                    imagerySet: 'CanvasLight',
-                },
-                {
-                    isVisible: false,
-                    imagerySet: 'CanvasGray',
-                }
-            ],
-            ol3d: undefined,
-            isCesiumFs: false,
-            pointsVector: undefined,
-            draw: undefined,
-            drawTypes: ['None', 'LineString', 'Polygon', 'Circle', 'Point'],
-            drawType: 'None',
-            geolocation: undefined
-        }
-    },
-    mounted() {
-        const layers = [];
-        for (let layer of this.layers) {
-            layers.push(
-                new TileLayer({
-                    visible: layer.isVisible,
-                    preload: Infinity,
-                    source: new BingMaps({
-                        key: 'AmVsq9RuMAPsJd1ASWEyfQEU5izCXRwGG1vaIw5eZ_nKoiCxZqN9PPZYoYYsFGQS',
-                        imagerySet: layer.imagerySet,
-                        culture: 'ru',
-                        maxZoom: 20
+    setup() {
+        const bingMapsKey = 'AmVsq9RuMAPsJd1ASWEyfQEU5izCXRwGG1vaIw5eZ_nKoiCxZqN9PPZYoYYsFGQS';
+
+        const isCesiumEnabled = ref(false);
+        const isCesiumFs = ref(false);
+        let ol3d = undefined;
+
+        const map2d = ref(null);
+        const bingMapsLayers = [
+            {
+                isVisible: true,
+                imagerySet: 'RoadOnDemand',
+            },
+            {
+                isVisible: false,
+                imagerySet: 'Aerial',
+            },
+            {
+                isVisible: false,
+                imagerySet: 'AerialWithLabelsOnDemand',
+            },
+            {
+                isVisible: false,
+                imagerySet: 'CanvasDark',
+            },
+            {
+                isVisible: false,
+                imagerySet: 'CanvasLight',
+            },
+            {
+                isVisible: false,
+                imagerySet: 'CanvasGray',
+            }
+        ];
+        const draw = ref(null);
+        const drawTypes = ref(['None', 'LineString', 'Polygon', 'Circle', 'Point']);
+        const drawType = ref('None');
+        const uploadGeoJSONInput = ref(null);
+        let view = undefined;
+        let drawingLayer = undefined;
+        let drawingVector = undefined;
+        let geolocation = undefined;
+
+        onMounted(async () => {
+            const mapLayers = [];
+            for (let layer of bingMapsLayers) {
+                mapLayers.push(
+                    new TileLayer({
+                        title: layer.imagerySet,
+                        visible: layer.isVisible,
+                        baseLayer: true,
+                        source: new BingMaps({
+                            key: bingMapsKey,
+                            imagerySet: layer.imagerySet,
+                            projection: 'EPSG:3857',
+                            culture: 'ru',
+                            maxZoom: 22
+                        })
                     })
+                );
+            }
+
+            view = new View({
+                center: [-11158582, 4813697],
+                zoom: 4
+            });
+            map2d.value = new Map({
+                layers: mapLayers,
+                controls: defaultControls().extend([
+                    new FullScreen(),
+                    new MousePosition({
+                        coordinateFormat: createStringXY(4),
+                        projection: 'EPSG:3857'
+                    })
+                ]),
+                target: 'map2d',
+                view: view,
+                loadTilesWhileAnimating: true,
+                loadTilesWhileInteracting: true
+            })
+
+            drawingVector = new SourceVector({
+                features: [],
+                wrapX: false
+            });
+            drawingLayer = new VectorLayer({
+                title: 'Custom Drawings',
+                source: drawingVector,
+            });
+            drawingLayer.set('name', 'drawingVector');
+            map2d.value.addLayer(drawingLayer);
+
+            geolocation = new Geolocation({
+                trackingOptions: {
+                    enableHighAccuracy: true,
+                },
+                projection: view.getProjection()
+            });
+
+            const accuracyFeature = new Feature();
+            geolocation.on('change:accuracyFeature', function () {
+                accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+            });
+            const positionFeature = new Feature();
+            positionFeature.setStyle(
+                new Style({
+                    image: new CircleStyle({
+                        radius: 6,
+                        fill: new Fill({
+                            color: '#3399CC',
+                        }),
+                        stroke: new Stroke({
+                            color: '#fff',
+                            width: 2,
+                        }),
+                    }),
                 })
             );
-        }
-        let view = new View({
-            center: [-11158582, 4813697],
-            zoom: 4
-        });
-        this.map = new Map({
-            controls: defaultControls().extend([
-                new FullScreen(),
-                new MousePosition({
-                    coordinateFormat: createStringXY(4),
-                    projection: 'EPSG:3857'
-                })
-            ]),
-            layers: layers,
-            target: 'map2d',
-            view: view
-        })
-
-        this.pointsVector = new SourceVector({
-            features: [
-                new Feature({
-                    geometry: new Point(fromLonLat([16.62662018, 49.2125578]))
-                })
-            ],
-            wrapX: false
-        });
-        let layer = new LayerVector({
-            source: this.pointsVector,
-        });
-        layer.set('name', 'pointsVector');
-        this.map.addLayer(layer);
-        const geolocation = new Geolocation({
-            trackingOptions: {
-                enableHighAccuracy: true,
-            },
-            projection: view.getProjection()
-        });
-        document.getElementById('trackLocation').addEventListener('click', function () {
-            // geolocation.setTracking(true);
-        });
-        const accuracyFeature = new Feature();
-        geolocation.on('change:accuracyFeature', function () {
-            accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
-        });
-        const positionFeature = new Feature();
-        positionFeature.setStyle(
-            new Style({
-                image: new CircleStyle({
-                    radius: 6,
-                    fill: new Fill({
-                        color: '#3399CC',
-                    }),
-                    stroke: new Stroke({
-                        color: '#fff',
-                        width: 2,
-                    }),
-                }),
-            })
-        );
-        geolocation.on('change:position', function () {
-            const coordinates = geolocation.getPosition();
-            positionFeature.setGeometry(coordinates ? new Point(coordinates) : null);
-        });
-        this.geolocation = geolocation;
-        this.pointsVector.addFeatures([accuracyFeature, positionFeature]);
-
-        const ol3d = new OLCesium({map: this.map, target: 'map3d'});
-        const scene = ol3d.getCesiumScene();
-        scene.shadowMap.enabled = true;
-        scene.terrainProvider = Cesium.createWorldTerrain();
-        // scene.imageryLayers.addImageryProvider(Cesium.createWorldImagery());
-        scene.primitives.add(Cesium.createOsmBuildings());
-        ol3d.setEnabled(this.isEnabled);
-        this.ol3d = ol3d;
-
-        document.getElementById('enable').addEventListener('click', () => {
-            ol3d.setEnabled(!ol3d.getEnabled())
-            this.isEnabled = !this.isEnabled
-            console.log(this.isEnabled)
-        })
-        console.log(this.$refs.toggleOlFsButton);
-    },
-    methods: {
-        onLayerChange() {
-            this.map.getLayers().forEach((layer, index) => {
-                layer.setVisible(this.pickedStyle === this.layers[index]?.imagerySet);
+            geolocation.on('change:position', function () {
+                const coordinates = geolocation.getPosition();
+                positionFeature.setGeometry(coordinates ? new Point(coordinates) : null);
             });
-            this.map.getLayers().getArray().find(layer => layer.get('name') === 'pointsVector').setVisible(true);
-        },
-        enableCesiumFs() {
-            this.isCesiumFs = !this.isCesiumFs;
-            if (this.isCesiumFs) {
+            drawingVector.addFeatures([accuracyFeature, positionFeature]);
+
+            // Cesium setup
+            ol3d = new OLCesium({map: map2d.value, target: 'map3d'});
+            const scene = ol3d.getCesiumScene();
+            scene.shadowMap.enabled = true;
+            scene.debugShowFramesPerSecond = true;
+            scene.terrainProvider = await Cesium.createWorldTerrainAsync();
+            // scene.imageryLayers.addImageryProvider(Cesium.createWorldImagery());
+            const osmBuildingsTileset = await Cesium.createOsmBuildingsAsync();
+            osmBuildingsTileset.style = new Cesium.Cesium3DTileStyle({
+                color: {
+                    conditions: [
+                        [true, "color('white', 0.85)"]
+                    ]
+                }
+            });
+            scene.primitives.add(osmBuildingsTileset);
+            ol3d.setEnabled(isCesiumEnabled.value);
+
+            // Sources need to become 'ready' before the tiles can be accessed and layer.getPreview working
+            map2d.value.addControl(new LayerSwitcherImage());
+        })
+
+        const onEnableCesiumButtonClick = () => {
+            ol3d.setEnabled(!ol3d.getEnabled())
+            isCesiumEnabled.value = !isCesiumEnabled.value
+        }
+
+        const enableCesiumFs = () => {
+            isCesiumFs.value = !isCesiumFs.value;
+            if (isCesiumFs.value) {
                 Cesium.Fullscreen.requestFullscreen(document.getElementById('map3d'));
             } else {
                 Cesium.Fullscreen.exitFullscreen();
             }
-        },
-        getRandomFloat(min, max) {
+        }
+
+        const getRandomFloat = (min, max) => {
             return Math.random() * (max - min) + min;
-        },
-        addRandomPoint() {
-            this.pointsVector.addFeature(new Feature({
-                geometry: new Point(fromLonLat([this.getRandomFloat(0, 50), this.getRandomFloat(0, 50)]))
+        }
+
+        const addRandomPoint = () => {
+            drawingVector.addFeature(new Feature({
+                geometry: new Point(fromLonLat([getRandomFloat(0, 50), getRandomFloat(0, 50)]))
             }));
-        },
-        removeRandomPoint() {
-            let features = this.pointsVector.getFeatures();
+        }
+
+        const removeRandomPoint = () => {
+            let features = drawingVector.getFeatures();
             if (features) {
-                this.pointsVector.removeFeature(features[0]);
+                drawingVector.removeFeature(features[0]);
             }
-        },
-        enableDrawing() {
-            this.map.removeInteraction(this.draw);
-            if (this.drawType !== 'None') {
-                this.draw = new Draw({
-                    source: this.pointsVector,
-                    type: this.drawType
+        }
+
+        const enableDrawing = () => {
+            map2d.value.removeInteraction(draw.value);
+            if (drawType.value !== 'None') {
+                draw.value = new Draw({
+                    source: drawingVector,
+                    type: drawType.value
                 });
-                this.map.addInteraction(this.draw);
+                map2d.value.addInteraction(draw.value);
             }
-        },
-        trackLocation() {
-           this.geolocation.setTracking(!this.geolocation.getTracking());
-            console.log(this.geolocation.getPosition());
-            this.map.getView().setCenter(this.geolocation.getPosition());
-            this.map.getView().setZoom(15);
+        }
+
+        const trackLocation = () => {
+            geolocation.setTracking(!geolocation.getTracking());
+            map2d.value.getView().setCenter(geolocation.getPosition());
+            map2d.value.getView().setZoom(15);
+        }
+
+        const saveAsGeoJSON = () => {
+            const geoJson = new GeoJSON().writeFeatures(drawingVector.getFeatures());
+            let a = document.createElement('a');
+            let file = new Blob([JSON.stringify(geoJson)], {type: 'application/json'});
+            a.href = URL.createObjectURL(file);
+            a.download = 'GeoJSON_Example.json';
+            a.click();
+            a.remove();
+        }
+
+        const uploadGeoJSON = (event) => {
+            const geoJsonFile = event.target.files[0];
+            const reader = new FileReader();
+            reader.onload = (res) => {
+                const geojson = JSON.parse(res.target.result);
+                map2d.value.addLayer(new VectorLayer({
+                    title: 'Imported',
+                    source: new SourceVector({
+                        features: new GeoJSON().readFeatures(geojson)
+                    })
+                }));
+            }
+            reader.readAsText(geoJsonFile);
+        }
+
+        const onGeoJSONUploadButtonClicked = () => {
+            uploadGeoJSONInput.value.click();
+        }
+
+        return {
+            isCesiumEnabled,
+            isCesiumFs,
+            drawType,
+            drawTypes,
+            uploadGeoJSONInput,
+            enableCesiumFs,
+            addRandomPoint,
+            removeRandomPoint,
+            enableDrawing,
+            trackLocation,
+            onEnableCesiumButtonClick,
+            saveAsGeoJSON,
+            onGeoJSONUploadButtonClicked,
+            uploadGeoJSON
         }
     }
 }
@@ -298,29 +371,23 @@ export default {
     height: 95%;
 }
 
-.layer_select {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    z-index: 1;
-}
-
 #map3d {
     background-color: #c0c0c0;
 }
 
-#enable {
+.cesium-buttons {
     position: absolute;
-    right: 10px;
-    top: 10px;
     z-index: 1;
 }
 
-.fs-button {
-    position: absolute;
+.cesium-enable-button {
+    right: 10px;
+    top: 10px;
+}
+
+.cesium-fs-button {
     left: 10px;
     top: 10px;
-    z-index: 1;
 }
 
 .centerRow {
@@ -329,25 +396,3 @@ export default {
 }
 
 </style>
-
-// or you can use url: https://{a-c}.tile.opentopomap.org/{z}/{x}/{y}.png
-// new TileLayer({
-//     title: 'OSM',
-//     type: 'base',
-//     visible: true,
-//     source: new XYZ({
-//         attributions: 'Tiles © <a href="https://opentopomap.org/">OpenTopoMap</a>',
-//         url: `https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${mapTilerKey}`,
-//         tileSize: 512,
-//         format: 'image/png',
-//         projection,
-//         style: 'default',
-//         tileGrid: new WMTSTileGrid({
-//             matrixIds,
-//             resolutions,
-//             origin: getTopLeft(projectionExtent)
-//         }),
-//         wrapX: true,
-//         crossOrigin: 'anonymous'
-//     })
-// }),
